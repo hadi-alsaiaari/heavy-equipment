7a971094d1116c6764464df5b10c0c1ac83e60af
hadi-alsaiaari <hadis.alsaiaari@gmail.com>
2025-09-01 22:22:40 +0300

    The dashboard theme has been prepared

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+    ],
+    function () {
+        Route::get('/', function () {
+            return view('dashboard.index');
+        });
+    }
+);
+
 
 Auth::routes();
 
